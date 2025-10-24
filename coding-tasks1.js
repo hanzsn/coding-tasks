13cc3d1e79361f84bdc4e4a13393b7cc1377b5e2
Hanz Lagonero <hanzlagonero@gmail.com>
2025-10-24 22:24:39 +0800

    rename

--- coding-tasks1.js
+++
@@ -1,6 +0,0 @@
-const twoString = ["hello", "world"];
-const twoStringConcatenate = twoString.join(" ");
-
-console.log(twoStringConcatenate);
-
-// declare 2 string variables, 1 is hello, 1 is world, concatenate to say hello world
--- coding-tasks2.js
+++
@@ -1,6 +0,0 @@
-const name = "     shanie     ";
-const trimWhiteSpace = name.trim();
-
-console.log(trimWhiteSpace);
-
-// Trim whitespace from strings using built in function
