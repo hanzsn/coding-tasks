5c39a05cfe1b1286d242dcfd8901535dbb66d5a8
Hanz Lagonero <hanzlagonero@gmail.com>
2025-10-24 22:17:46 +0800

    added a comment

--- coding-tasks1.js
+++ coding-tasks1.js
@@ -2,3 +2,5 @@ const twoString = ["hello", "world"];
 const twoStringConcatenate = twoString.join(" ");
 
 console.log(twoStringConcatenate);
+
+// declare 2 string variables, 1 is hello, 1 is world, concatenate to say hello world
